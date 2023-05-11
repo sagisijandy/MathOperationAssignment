9b1488c4b9090ae099fc8e7ba96f4bccc0125df4
sagisijandy <98680878+sagisijandy@users.noreply.github.com>
2023-05-11 11:31:45 -0700

    Add project files.

---
+++ MathOperationAssignment/MathOperation.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class MathOperations
+{
+    public void MathOperation(int num1, int num2)
+    {
+        int result = num1 + 5; // do a math operation on the first integer
+        Console.WriteLine(num2); // display the second integer to the screen
+    }
+}
---
+++ MathOperationAssignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MathOperationAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperations math = new MathOperations(); // instantiate the class
+
+            math.MathOperation(5, 2); // call the method, passing in two numbers
+            math.MathOperation(num2: 2, num1: 5); // call the method, specifying the parameters by name
+        }
+    }
+}
+
+   
